28504a2e4f6e88c0a95e9ca6502ad35df0778a73
Ryan Catrine <ryancatrine@gmail.com>
2019-03-04 12:25:42 -0500

    Added code to generate a map png with assistance from Rob.

--- pathfinder.py
+++ pathfinder.py
@@ -8,59 +8,45 @@ class Map:
         with open(filename) as file:
             for line in file:
                 self.elevations.append([int(e) for e in line.split()])
-        """Finds max/min elevation points with 'generators'?"""   
+        """Finds max/min elevation point in each list with generators"""   
         self.max_elevation = max([max(row) for row in self.elevations])
         self.min_elevation = min([min(row) for row in self.elevations])
 
 
-def get_elevation(self, x, y):
-    """Switches x and y coordinates for 'readability'?"""
-    return self.elevations[y][x]
+    def get_elevation(self, x, y):
+        """Switches x and y coordinates for readability"""
+        return self.elevations[y][x]
 
 
-def get_intensity(self, x, y):
-    """Gets elevation color intensity"""
-    return self.get_elevation(x, y) / self.max_elevation * 255
-    return (self.get_elevation(x, y) - self.min_elevation) / (
-        self.max_elevation - self.min_elevation) * 255
+    def get_intensity(self, x, y):
+        """Gets elevation color intensity"""
+        return int((self.get_elevation(x, y) - self.min_elevation) / (
+            self.max_elevation - self.min_elevation) * 255)
 
 
-def __init__get_map:(self, x, y):
-    """Plots colored pixels in a new image creating a map, saves the new image"""
-    
-    im = Image.new('RGBA', (600, 600))
+    def get_map(self):
+        """Plots colored pixels in a new image creating a map, saves the new image"""
+        
+        mountain_map = Image.new('RGB', (600, 600))
 
-    im.getpixel((0, 0))
+        mountain_map.getpixel((0, 0))
 
-    for x in range(600):
-        for y in range(600):
-            im.putpixel((x, y), ("""color intensity"""))
-    from PIL import ImageColor
-    for x in range(600):
-        for y in range
+        for x in range(600):
+            for y in range(600):
+                mountain_map.putpixel((x, y), (self.get_intensity(x, y), self.get_intensity(x, y), self.get_intensity(x, y)))
+        mountain_map.save("mountain_map.png")
+        
     
 
-# """Draw colored pixel route of lowest-elevation-change"""
 # class DrawRoute:
+# """Draw colored pixel route of lowest-elevation-change"""
 #     def __init__(self, ):
 
 
 
 if __name__ == "__main__":
     map_data = Map("elevation_small.txt") 
-    
-
-
-# from PIL import Image
-    # img = Image.new("RGB", (600, 600), (255, 255, 255))
-    # draw = ImageDraw.Draw(img)
-    # square = Square((50, 50), 30)
-    # draw.rectangle([
-    #     square.origin,
-    #     (square.origin[0]) + square.width, square.origin[1] + square.length)],
-    #                 fill=0
-    # img.save("test.jpg", "JPEG")
-
-
-
+    # get_elevation = map_data.get_elevation
+    # get_intensity = map_data.get_intensity
+    map_data.get_map()
 
